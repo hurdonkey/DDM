2665bfe50597840d3af37bc0d8a1d90d92cb9699
donky <liang.zhang@gemii.cc>
2019-05-29 12:01:21 +0800

    update show effect

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
-""" Donkys Downloader by multi-range """
+""" Donkys Downloader by multi-range
+使用http range和ftp REST请求的分段下载脚本
+"""
+
 
 import os
-from multiprocessing import Process, Pool, Manager, Queue, Lock, cpu_count
-import requests
+import time
+import multiprocessing
 import argparse
+import requests
 from abc import ABCMeta, abstractmethod, abstractstaticmethod
 
+
+BUFFSIZE = 1024 * 1
 
 class Downloader():
     __metaclass__ = ABCMeta
@@ -29,6 +35,8 @@ class Downloader():
 
     def split_range(self):
         """ 按照目标大小和任务数对目标分片 """
+        # length = 100
+        # 分片 (0 ~ a), (a+1, 99)
         self._get_length()
         splited_range_list = []
         delta = self.length // self.task_num
@@ -39,7 +47,7 @@ class Downloader():
             range_tuple = (start, end)
             splited_range_list.append(range_tuple)
             cursor = end+1
-        splited_range_list.append((cursor, self.length))
+        splited_range_list.append((cursor, self.length-1))
         return splited_range_list
 
 
@@ -69,11 +77,15 @@ class HTTPDownloader(Downloader):
         })
         print(response.headers)
         self.length = int(response.headers['Content-Length'])
-        print(self.length)
+        # print(self.length)
 
-    def run_splited_task(self, lock, range_start, range_end):
+    def run_splited_task(self, target_lock, size_done_array, size_full_array, process_ordinal, range_start, range_end):
         pid = os.getpid()
-        print("run task: %s-%s, process id: %s." % (range_start, range_end, pid))
+        size_full_array[process_ordinal] = (range_end - range_start + 1)
+        print("run task %d, range: %s-%s, process id: %s." % (
+            process_ordinal, range_start, range_end, pid
+        ))
+
         try:
             # 必须捕获 否则进程异常完全没有提示
             # 捕获手动输出异常也是有必要的
@@ -82,17 +94,14 @@ class HTTPDownloader(Downloader):
                 'Range': 'bytes=%s-%s' % (range_start, range_end),
                 'accept-encoding': 'gzip;q=0,deflate,sdch',
             })
-            content = response.content
-
             offset = range_start
+            
+            with target_lock:
+                # print("process %s get lock" % pid)
 
-            # self.target_file_path = self.target_file_path + '_' + str(pid)
-            print("%s get lock" % pid)
-                
-            with lock:
                 with open(self.target_file_path,'a'):
-                    # 创建空文件
-                    # 后面必须用r+ 但是r+不能创建文件
+                    # 手动创建空文件
+                    # 后面写入必须用r+ 但是r+不能创建文件
                     pass
 
                 with open(self.target_file_path, "rb+") as fd_target:
@@ -100,21 +109,28 @@ class HTTPDownloader(Downloader):
                     # w回使用新文件, 之前线程的写入丢失
                     # a只能从尾部修改, 不能修改已存在的部分
                     fd_target.seek(offset, os.SEEK_SET)
-                    fd_target.write(content)
+                    buff_iter = response.iter_content(chunk_size=BUFFSIZE)
+                    for buff in buff_iter:
+                        fd_target.write(buff)
 
-                filesize = os.path.getsize(self.target_file_path)
-                print("now file size:", filesize)
+                        # recode downloaded size
+                        # size_done.value += len(buff)
+                        # print('size downloaded: %d' % size_done.value)
 
-                # with open(self.target_file_path, "rb") as fd_target:
-                #     print(fd_target.read())
+                        size_done_array[process_ordinal] += len(buff)
 
-            # TODO: recode downloaded size
-            print("process %s ok, datalen: %s" % (pid, len(content)))
+                # 测试文件当前大小
+                # filesize = os.path.getsize(self.target_file_path)
+                # print("now file size:", filesize)
+
+                # print("process %s release lock" % pid)
 
         except Exception as e:
             print(e)
             # TODO: 被主进程捕获 让主进程重新调用此任务
 
+
+        print("process %s ok!" % pid)
 
 def optioninit():
     """ get check args and  options """
@@ -137,7 +153,7 @@ def main():
     url = "http://download.firefox.com.cn/releases/firefox/42.0/zh-CN/Firefox-latest-x86_64.tar.bz2"
     # url = "http://192.168.137.10"
     output = "a.out"
-    tasks = 3
+    tasks = 2
 
     # 区别下载类型 声明下载对象
     # if "ftp:":
@@ -150,27 +166,57 @@ def main():
     splited_range_list = downloader.split_range()
     print(splited_range_list)
 
-    # 先打开文件 而不是在每个进程里都打开
-    # fd_target = open(output, 'wb')
-
     # 创建进程池
-    lock = Manager().Lock()
-    # queue = Manager().Queue()
-    max_process_num = cpu_count() * 2
+    max_process_num = multiprocessing.cpu_count() * 2
     process_num = tasks if tasks < max_process_num else max_process_num
-    process_pool = Pool(process_num)
+    process_pool = multiprocessing.Pool(process_num)
+
+    # 目标文件 写入锁
+    target_lock = multiprocessing.Manager().Lock()
+
+    # 共享Array 保存每个进程 自己下载任务的的已完成长度(size_done_array) 和 总长度(size_full_array)
+    size_done_array = multiprocessing.Manager().Array('I', [0]*tasks)
+    size_full_array = multiprocessing.Manager().Array('I', [0]*tasks)
 
     # 加入任务
+    process_ordinal = 0   # 进程序号 和共享Array的位置相对应
     for splited_range in splited_range_list:
         process_pool.apply_async(
             downloader.run_splited_task,
-            args=(lock,) + splited_range
+            args=(target_lock, size_done_array, size_full_array, process_ordinal) + splited_range
         )
+        process_ordinal += 1
+
     process_pool.close()
 
     # 当前进程主线程打印下载进度
-    process_pool.join()
+    # process_pool.join()
+    while True:
+        # print(size_full_array)
+        # print(size_done_array)
 
+        # 完成的任务数
+        done_count = 0
+        for i in range(len(size_full_array)):
+            current_size = size_done_array[i]
+            total_size = size_full_array[i]
+
+            print(' || %d size downloaded: %d' % (
+                i,
+                current_size,
+                #  , %.2f %% (current_size * 100 / total_size)
+            ), end='', flush=True)
+
+            if current_size >= total_size and total_size != 0:
+                done_count += 1
+
+        print()
+
+        if done_count == len(size_full_array):
+            print('Downloaded well done!')
+            break
+
+        time.sleep(0.2)
 
 if __name__ == "__main__":
     main()
